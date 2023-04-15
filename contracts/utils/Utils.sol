a0b5c6af7c600cbd3412b97c981deef9489b20cd
yungs1717 <yungs1717@users.noreply.github.com>
2023-04-16 03:55:21 +0900

    misc

--- contracts/utils/Utils.sol
+++ contracts/utils/Utils.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.0;
 
 library Utils {
-//    function compareStringHashes(
-//        string memory hash1, string memory hash2
-//    ) external pure returns (bool) {
-//         return (keccak256(bytes(hash1)) == keccak256(bytes(hash2)));
-//    }
+    //    function compareStringHashes(
+    //        string memory hash1, string memory hash2
+    //    ) external pure returns (bool) {
+    //         return (keccak256(bytes(hash1)) == keccak256(bytes(hash2)));
+    //    }
 }
