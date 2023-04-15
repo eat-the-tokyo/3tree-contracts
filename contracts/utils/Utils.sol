41acbdb165d32b1491d42696ea2bdc6e98af9606
yungs1717 <yungs1717@users.noreply.github.com>
2023-04-15 16:36:40 +0900

    add Utils

---
+++ contracts/utils/Utils.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+library Utils {
+//    function compareStringHashes(
+//        string memory hash1, string memory hash2
+//    ) external pure returns (bool) {
+//         return (keccak256(bytes(hash1)) == keccak256(bytes(hash2)));
+//    }
+}
